1489b361d07b3abe208fa3626acdf6e2b68f703c
julbroersen <julianabroersen2@gmail.com>
2025-02-21 18:20:30 +0100

    added ai api and set up poem generator in javascript

--- src/script.js
+++ src/script.js
@@ -1,14 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector("#poem");
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "Generating poem...",
+    strings: response.data.answer,
     autoStart: true,
     delay: 20,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "63fd08b40a55a7od498ecfac7t28e16f";
+  let context = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let prompt =
+    "You are a poem expert who loves to write short, sassy, funny poems. Your mission is to generate a short, sassy, funny poem of maximum 4 lines. Each line should only have one phrase and be separated with a <br/>. The output should only be the 4 lines of poem, nothing else. Make sure to be polite and to follow the user instructions. Sign the poem at the end with 'xoxo Gossip Girl'";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
