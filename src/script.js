c3f283ecff1df4b29499fa8e092bfc5c9e3cc25b
julbroersen <julianabroersen2@gmail.com>
2025-03-27 14:08:43 +0100

    changed AI instructions

--- src/script.js
+++ src/script.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let apiKey = "63fd08b40a55a7od498ecfac7t28e16f";
   let context = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let prompt =
-    "You are a poem expert who loves to write short, sassy, funny poems. Your mission is to generate a short, sassy, funny poem of maximum 4 lines. Each line should only have one phrase and be separated with a <br/>. The output should only be the 4 lines of poem, nothing else. Make sure to be polite and to follow the user instructions. Sign the poem at the end with 'xoxo Gossip Girl'";
+    "You are a poem expert who loves to write short, sassy, funny poems. Your mission is to generate a short, sassy, funny poem of maximum 4 lines. Each line should only have one phrase and be separated with a <br/>. The output should only be the 4 lines of poem, nothing else. Make sure to be appropriate (nothing too offensive) and to follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
